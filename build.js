25af3dead56ec141d578e147ac2e6364cdde9193
Dan Prince <dan@codecurve.net>
2013-12-06 05:04:58 +0000

    Testing image processing

--- build.js
+++ build.js
@@ -1,15 +1,35 @@
 var themes = require('./src/themes');
+var palette = require('./src/palette');
 var handlebars = require('handlebars');
 var fs = require('fs');
+var gm = require('gm').subClass({imageMagick: true});
 
 var src = 'TEMPLATE.md';
 var dst = 'README.md';
+var dir = __dirname + '/themes/';
+
+var img = gm(dir + 'blank.png');
+console.log(img);
+
+for(var name in themes) {
+    (function(img) {
+        var colors = palette(name, 10);
+        
+        for(var i = 0; i < colors.length; i++) {
+            console.log(colors);
+            img.drawRectangle(i * 10, 0, 10, 10).fill(colors[i]);
+        }
+        
+        img.write(dir + name + '.png', function(err) {
+            if(err) throw err;
+        });
+    })(img);
+}
 
 // read in file
 var source = fs.readFileSync(src).toString();
 var template = handlebars.compile(source);
 var out = template({themes: themes});
 
-console.log(out);
 // write back to file
 fs.writeFileSync(dst, out);
